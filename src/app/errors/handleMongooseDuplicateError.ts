99e3b6c773cad4b1ccff6381333a63b8d7c14fdb
bayazidhassan <bayazidhassan776@gmail.com>
2025-09-23 04:42:07 +0600

    duplicate error message

--- src/app/errors/handleMongooseDuplicateError.ts
+++ src/app/errors/handleMongooseDuplicateError.ts
@@ -4,8 +4,9 @@ export const handleMongooseDuplicateError = (
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   err: any,
 ): TGenericErrorResponse => {
-  const match = err.message.match(/"([^"]*)"/);
-  const extractedMessage = match && match[1];
+  //E11000 duplicate key error collection: UNIVERSITY_BACKEND.students index: email_1 dup key: { email: "bayazid1@gmail.com" }
+  const match = err.message.match(/"([^"]*)"/); //matches text inside double quotes.
+  const extractedMessage = match && match[1]; //safely avoids errors if nothing is matched && returns the captured string.
 
   const errorSource: TErrorSource[] = [
     {
