0095cff0aebb3ffe0369a9f4117b99939b926793
bayazidhassan <bayazidhassan776@gmail.com>
2025-09-24 02:00:20 +0600

    duplicate error

--- src/app/errors/handleMongooseDuplicateError.ts
+++ src/app/errors/handleMongooseDuplicateError.ts
@@ -5,13 +5,19 @@ export const handleMongooseDuplicateError = (
   err: any,
 ): TGenericErrorResponse => {
   //E11000 duplicate key error collection: UNIVERSITY_BACKEND.students index: email_1 dup key: { email: "bayazid1@gmail.com" }
-  const match = err.message.match(/"([^"]*)"/); //matches text inside double quotes.
-  const extractedMessage = match && match[1]; //safely avoids errors if nothing is matched && returns the captured string.
+  //const match = err.message.match(/"([^"]*)"/); //matches text inside double quotes-> "bayazid1@gmail.com"
+  //const extractedMessage = match && match[1]; //safely avoids errors if nothing is matched && returns the captured string.
+
+  const path = Object.keys(err.keyPattern)[0]; //keyPattern: { email: 1 },
+  const value = err.keyValue[path]; //keyValue: { email: 'bayazid1@gmail.com' }
 
   const errorSource: TErrorSource[] = [
     {
-      path: '',
-      message: `${extractedMessage} is already exists`,
+      //path: '',
+      //message: `${extractedMessage} is already exists`,
+
+      path: path,
+      message: `${value} is already exists`,
     },
   ];
 
