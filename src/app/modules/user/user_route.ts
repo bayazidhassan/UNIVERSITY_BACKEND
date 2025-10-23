af53bd5d4a511f2c192e11cfdfe182bdf80ce77c
bayazidhassan <bayazidhassan776@gmail.com>
2025-10-24 05:26:04 +0600

    super_admin -> create_student route

--- src/app/modules/user/user_route.ts
+++ src/app/modules/user/user_route.ts
@@ -1,10 +1,10 @@
 import express, { NextFunction, Request, Response } from 'express';
 import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
+import { upload } from '../../utils/sendImageToCloudinary';
 import { adminValidation } from '../admin/admin_validation';
 import { facultyValidation } from '../faculty/faculty_validation';
 import { studentValidation } from '../student/student_validation';
-import { upload } from '../../utils/sendImageToCloudinary';
 import { userControllers } from './user_controller';
 import { USER_ROLE } from './user_interface';
 import { userValidation } from './user_validation';
@@ -13,10 +13,10 @@ const router = express.Router();
 
 router.post(
   '/create_student',
-  auth(USER_ROLE.admin),
-  upload.single('profile_image'), //postman-> form-data: File key name
+  auth(USER_ROLE.admin, USER_ROLE.super_admin),
+  upload.single('profile_image'), //postman-> form-data: File's key name
   (req: Request, res: Response, next: NextFunction) => {
-    req.body = JSON.parse(req.body.data); //to convert text data to json data
+    req.body = JSON.parse(req.body.data); //to convert text data to json data, postman-> form-data: Text's key name (data)
     next();
   },
   validateRequest(studentValidation.createStudentZodSchema),
