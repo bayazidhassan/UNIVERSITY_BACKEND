60be3e6a697d402fb20d79d5ac5c76a06457565d
bayazidhassan <bayazidhassan776@gmail.com>
2025-10-24 05:11:25 +0600

    auth->createAcademicFaculty

--- src/app/modules/academicFaculty/academicFaculty_route.ts
+++ src/app/modules/academicFaculty/academicFaculty_route.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
+import { USER_ROLE } from '../user/user_interface';
 import { academicFacultyController } from './academicFaculty_controller';
 import { academicFacultyValidation } from './academicFaculty_validation';
 
 const router = express.Router();
 router.post(
   '/createAcademicFaculty',
+  auth(USER_ROLE.super_admin),
   validateRequest(academicFacultyValidation),
   academicFacultyController.createAcademicFaculty,
 );
