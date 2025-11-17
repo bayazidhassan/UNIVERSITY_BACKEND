607c103970714c51990075e008349f0e7b764ee1
bayazidhassan <bayazidhassan776@gmail.com>
2025-11-17 23:03:46 +0600

    auth guard

--- src/app/modules/academicSemester/academicSemester_route.ts
+++ src/app/modules/academicSemester/academicSemester_route.ts
@@ -1,9 +1,9 @@
 import express from 'express';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
+import { USER_ROLE } from '../user/user_interface';
 import { academicSemesterController } from './academicSemester_controller';
 import { academicSemesterValidation } from './academicSemester_validation';
-import { USER_ROLE } from '../user/user_interface';
-import auth from '../../middlewares/auth';
 
 const router = express.Router();
 
@@ -13,7 +13,11 @@ router.post(
   validateRequest(academicSemesterValidation.createAcademicSemesterZodSchema),
   academicSemesterController.createAcademicSemester,
 );
-router.get('/', academicSemesterController.getAllAcademicSemester);
+router.get(
+  '/',
+  auth(USER_ROLE.super_admin, USER_ROLE.admin),
+  academicSemesterController.getAllAcademicSemester,
+);
 router.get('/:id', academicSemesterController.getAnAcademicSemester);
 
 router.patch(
